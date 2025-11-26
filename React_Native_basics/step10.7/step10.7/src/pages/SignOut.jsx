5a3a040b67cfb43a060e42a8d7028a82379b8e57
Nandini-Mane <nandinimane2005@gmail.com>
2025-11-26 16:43:12 +0530

    first-commit

--- React_Native_basics/step10.7/step10.7/src/pages/SignOut.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function SignOut() {
-  return (
-    <View>
-      <Text style={{ fontSize: 20, margin: 20 }}>Signed Out</Text>
-    </View>
-  );
-}
